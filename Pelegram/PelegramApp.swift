4e3e6711cf35813683ee807ef91ee92dcd5377ff
serhio <serhiooreh@gmail.com>
2023-07-07 21:21:39 +0300

    Initial Commit

---
+++ Pelegram/PelegramApp.swift
@@ -0,0 +1,17 @@
+//
+//  PelegramApp.swift
+//  Pelegram
+//
+//  Created by Serhii Orekhivskyi on 07.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PelegramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
